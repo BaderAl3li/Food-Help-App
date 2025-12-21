c5914fdf88935aaee161e450dbff82459a3f1f84
hasanali-poly <202304009@student.polytechnic.bh>
2025-12-21 11:09:02 +0300

    aa

---
+++ code3/ViewController.swift
@@ -0,0 +1,19 @@
+    //
+//  ViewController.swift
+//  code3
+//
+//  Created by BP-19-114-09 on 21/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ code3Tests/code3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  code3Tests.swift
+//  code3Tests
+//
+//  Created by BP-19-114-09 on 21/12/2025.
+//
+
+import Testing
+@testable import code3
+
+struct code3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
