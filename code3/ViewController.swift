bca4d0098caedf4a29f3000b0b8d7384492192a1
hasanali-poly <202304009@student.polytechnic.bh>
2025-12-22 12:00:23 +0300

    incomplete

--- code3/ViewController.swift
+++ code3/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import Cloudinary
+import SDWebImage
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var call_logo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guard let call_logo = call_logo else {return}
+        guard let url = URL (string: "https://res.cloudinary.com/daeudw63f/image/upload/v1766392780/image_10_jzg2fr.png")
+        else {print("bad url")
+            return
+        }
+        call_logo.sd_setImage(with: url, placeholderImage: UIImage(systemName: "photo"))
+                                    // Do any additional setup after loading the view.
     }
 
 
