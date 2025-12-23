26183891a4711a0e9ea24c6ce7addebdbbab2805
BaderAl3li <202303330@student.polytechnic.bh>
2025-12-23 12:29:01 +0300

    finalized ui

--- FoodHelpApp/FoodHelp/FoodHelp/AppDelegate.swift
+++ FoodHelpApp/FoodHelp/FoodHelp/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by BP-36-224-16 on 10/12/2025.
 //
-
+import FirebaseCore
 import UIKit
 
 @main
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
