6a82b1d7d7a3b8db9795a2b958541d36891ef8ea
Hussain <202304006@student.polytechnic.bh>
2025-12-29 16:04:24 +0300

    worked on the second feature
    i Started working on the second feature

---
+++ Signin/Signin/Leaderboard.swift
@@ -0,0 +1,29 @@
+//
+//  Leaderboard.swift
+//  Signin
+//
+//  Created by BP-19-131-02 on 29/12/2025.
+//
+
+import UIKit
+
+class Leaderboard: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Signin/Signin/ViewController.swift
+++ Signin/Signin/ViewController.swift
@@ -10,60 +10,24 @@ import Cloudinary
 import SDWebImage
 import FirebaseCore
 import FirebaseAuth
+import FirebaseFirestore
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    
-    @IBAction func signoutClicked(_ sender: UIButton) {
-        
-        let alert = UIAlertController(
-            title: "Are you sure?",
-            message: "Logging out requires to log in again. Are you sure you want to continue?",
-            preferredStyle: .alert
-        )
-
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-
-        alert.addAction(
-            UIAlertAction(title: "Sign out", style: .destructive) { _ in
-                do {
-                    try Auth.auth().signOut()
-                    self.navigateToLogin()
-                } catch {
-                    self.showAlert(title: "Signout Failed", message: "Retry in a few minutes")
-                }
-            }
-        )
-
-        present(alert, animated: true)
-
-    }
-    
-    func navigateToLogin(){
-        let sb = UIStoryboard(name: "Main", bundle: nil)
-        let loginVC = sb.instantiateViewController(withIdentifier: "Home")
-
-            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-               let window = windowScene.windows.first {
-                window.rootViewController = loginVC
-                window.makeKeyAndVisible()
-            }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         guard let imageView = imageView else {return}
         guard let url = URL(string: "https://res.cloudinary.com/dfc9jminy/image/upload/v1766385157/app_logo1_1_t4cvkh.png") else{
             print("Bad URL")
             return
         }
         imageView.sd_setImage(with: url, placeholderImage: UIImage(systemName: "photo"))
+        
     }
     
-
+    
         
     
     override func viewDidAppear(_ animated: Bool) {
@@ -72,13 +36,6 @@ class ViewController: UIViewController {
         
     }
     
-    func showAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alert, animated: true)
-    }
-    
-
 }
 
 
